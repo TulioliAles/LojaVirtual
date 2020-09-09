886a7f11ff69f1f8839fcd1b02cc78147f23a6d6
TulioliAles <desmecad@hotmail.com>
2020-09-09 20:55:01 -0300

    Commit 1 - Iniciado o projeto e criadas as páginas Home, Contato, Login, Carrinho e Cadastro de Clientes

---
+++ Controllers/ProdutoController.cs
@@ -0,0 +1,30 @@
+﻿using LojaVirtual.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LojaVirtual.Controllers
+{
+    public class ProdutoController : Controller
+    {
+       
+        public ActionResult Visualizar()
+        {
+            Produto produto = GetProduto();
+            return View(produto);
+        }
+
+        private Produto GetProduto()
+        {
+            return new Produto()
+            {
+                Id = 1,
+                Nome = "X Box",
+                Descricao = "Jogue em 4K",
+                Valor = 2000.00M
+            };
+        }
+    }
+}
